8fd08df45297f1631193a1c281a8ef64a1528044
MoizAnsari7 <moizansari176@gmail.com>
2025-08-12 16:10:01 +0530

    login page

---
+++ src/app/login/page.js
@@ -0,0 +1,13 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const LoginPage = dynamic(() => import('@/components/Login/LoginPage'), {
+  ssr: false,
+});
+
+export default function Login() {
+  return (
+    <LoginPage />
+  );
+}
